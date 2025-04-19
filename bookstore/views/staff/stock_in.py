e0ef690d01133461876d82e6fba644c93d4d4243
hongchi <chiautowin2225@gmail.com>
2025-04-19 21:01:45 +0700

    aaaa

--- bookstore/views/staff/stock_in.py
+++ bookstore/views/staff/stock_in.py
@@ -2,21 +2,23 @@ from django.contrib import admin
 from ...models import StockIn
 
 class StockInAdmin(admin.ModelAdmin):
-    list_display = ('book', 'quantity', 'date', 'note')
+    def total_price(self, obj):
+        return obj.price * obj.quantity
+    
+    total_price.short_description = 'total_price'  # Đặt tên hiển thị cho cột
+
+    list_display = ('book', 'quantity', 'total_price', 'date', 'note')
     list_filter = ('book', 'date')
     search_fields = ('book__title',)
     ordering = ('-date',)
-    fields = ('book', 'quantity', 'note')
-    readonly_fields = ('date',)
+    fields = ('book', 'quantity', 'note','price')
+    readonly_fields = ('date', 'total_price')
 
     def has_change_permission(self, request, obj=None):
-            return False
-
+        return False
 
     def has_view_permission(self, request, obj=None):
-        # Cho phép xem tất cả bản ghi
         return True
 
     def has_add_permission(self, request):
-        # Cho phép tạo bản ghi mới
         return True
