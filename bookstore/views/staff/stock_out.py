8f9a161ebdac83afb683ca7ae2801638e1cf2d8b
hongchi <chiautowin2225@gmail.com>
2025-04-19 23:22:40 +0700

    aaaa

--- bookstore/views/staff/stock_out.py
+++ bookstore/views/staff/stock_out.py
@@ -5,22 +5,43 @@ class StockOutAdmin(admin.ModelAdmin):
     def total_price(self, obj):
         return obj.book.price * obj.quantity
     
-    total_price.short_description = 'Total Price'  # Đặt tên hiển thị cho cột
+    def book_price(self, obj):
+        return obj.book.price
 
-    list_display = ('id',  'total_price','book' ,'date', 'order_id')
-    list_filter = ('book', 'date', 'order_id')
-    search_fields = ('book__title',)
+    def order_id(self, obj):
+        return obj.order.id if obj.order else None
+    # order_id.short_description = 'Order ID'
+
+    list_display = ('id', 'book', 'book_price', 'quantity', 'total_price', 'date', 'note', 'order_id')
+    list_filter = ('book', 'date')
+    search_fields = ('book__title', 'order__id')
     ordering = ('-date',)
-    fields = ('book', 'quantity','total_price' ,'note' )
-    readonly_fields = ('date',)
+    fields = ('book', 'book_price', 'quantity', 'note')
+    readonly_fields = ('date', 'total_price', 'book_price')
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        # Lọc StockOut khi Order có status là "Confirmed" hoặc "Completed"
+        return queryset.filter(order__status__in=['Confirmed', 'Completed']).select_related('book', 'order')
 
     def has_change_permission(self, request, obj=None):
-            return False
+        return False
 
     def has_view_permission(self, request, obj=None):
-        # Cho phép xem tất cả bản ghi
         return True
 
     def has_add_permission(self, request):
-        # Cho phép tạo bản ghi mới
-        return True
+        return False  # Chặn tạo thủ công vì StockOut được tạo tự động
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ['book', 'quantity', 'date', 'note', 'total_price']
+        return ['date', 'total_price']
+
+    def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        if object_id:
+            extra_context['show_save'] = False
+            extra_context['show_save_and_continue'] = False
+            extra_context['show_delete'] = False
+        return super().changeform_view(request, object_id, form_url, extra_context)
